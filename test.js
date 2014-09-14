e0c022e2c045a06e877c4d47cf3d35bff2b6f3fd
shortsn <shortsn>
2014-09-14 12:18:57 +0200

    test

---
+++ test.js
@@ -0,0 +1,34 @@
+var gpio = require('rpi-gpio');
+var async = require('async');
+
+async.parallel([
+    function(callback) {
+        gpio.setup(15, gpio.DIR_OUT, callback);
+    },
+], function(err, results) {
+	console.log(err);
+  console.log('Pins set up');
+	write();
+});
+
+function write() {
+    async.series([
+        function(callback) {
+            delayedWrite(15, true, callback);
+        },
+    ], function(err, results) {
+        console.log('Writes complete, pause then unexport pins');
+        setTimeout(function() {
+            gpio.destroy(function() {
+                console.log('Closed pins, now exit');
+                return process.exit(0);
+            });
+        }, 500);
+    });
+}
+
+function delayedWrite(pin, value, callback) {
+    setTimeout(function() {
+        gpio.write(pin, value, callback);
+    }, 2000);
+}
