1ee5daba110dcd2a9c1421612f793da738fd4b20
shortsn <shortsn>
2014-11-16 14:48:42 +0100

    observeGPIO

--- rx_test.js
+++ rx_test.js
@@ -4,14 +4,16 @@ var exec = require('child_process').exec;
 var GPIO = require('onoff').Gpio;
 
 function observeGPIO(number, edge) {
-  exec('gpio-admin export '+ number, function() {
-    var gpio = new GPIO(number, 'in', edge);
-    var last_value;
+  var last_value;
 
-    var input_stream = Rx.Observable.create(function (observer) {
+  var input_stream = Rx.Observable.create(function (observer) {
+    exec('gpio-admin export '+ number, function() {
+      var gpio = new GPIO(number, 'in', edge);
+
       gpio.read(function(err, value) {
         if (err) throw err;
         last_value = value;
+        observer.onNext(value);
 
         gpio.watch(function(err, value) {
           last_value = value;
@@ -23,23 +25,27 @@ function observeGPIO(number, edge) {
         console.log('unexport gpio '+ number);
         gpio.unexport();
       };
-    })
-    .publish()
-    .refCount();
 
-    return Rx.Observable.return(last_value).concat(input_stream);
-  });
+    });
+  })
+  .publish()
+  .refCount();
+
+  return Rx.Observable
+  .return(last_value)
+  .concat(input_stream)
+  .where(function(value) { return value !== undefined; });
 }
 
 var button = observeGPIO(18, 'both');
 
 button.subscribe(
-    function (x) {
-        console.log('Next: ' + x.toString());
-    },
-    function (err) {
-        console.log('Error: ' + err);
-    },
-    function () {
-        console.log('Completed');
-    });
+  function (x) {
+    console.log('Next: ' + x);
+  },
+  function (err) {
+    console.log('Error: ' + err);
+  },
+  function () {
+    console.log('Completed');
+  });
